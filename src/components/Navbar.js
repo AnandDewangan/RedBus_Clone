99f89ad1aceb79b748ccc70186784af8c2b442f4
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-02-19 05:50:59 +0530

    some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,13 +10,12 @@ function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="danger" variant="dark" className='fixed-top'>
       <Container className='container'>
-        <Navbar.Brand href="/"><Image src={'https://www.redbus.in/i/59538b35953097248522a65b4b79650e.png'}/></Navbar.Brand>
+        <Navbar.Brand href="/home"><Image src={'https://www.redbus.in/i/59538b35953097248522a65b4b79650e.png'}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <NavDropdown title={<FaRegUser/>} id="collasible-nav-dropdown" style={{position:"absolute", right:'8%'}}>
-              <NavDropdown.Item href="/login">SignIn/SignUp</NavDropdown.Item>
+              <NavDropdown.Item href="/">SignIn/SignUp</NavDropdown.Item>
             </NavDropdown>
-          <Nav.Link href="/help" style={{color:'#fff', position:"absolute", right:'5%'}}>Help</Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
